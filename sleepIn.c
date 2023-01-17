9e0f7f248ac6373efe8bfe7ecf5d62f8d255f018
MythicalCow <62105787+MythicalCow@users.noreply.github.com>
2023-01-17 11:33:10 -0500

    sleepIn

---
+++ sleepIn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#define TRUE 1
+#define FALSE 0
+
+int sleepIn(int weekday, int vacation){
+    return !weekday || vacation;
+}
+//char takes up 1 byte
+int main() {
+    int x = sleepIn(FALSE, FALSE);
+    char *sleepIn = malloc(6);
+    if(x==0){
+        strcpy(sleepIn, "FALSE");   
+    }
+    else{
+        strcpy(sleepIn, "TRUE");
+    }
+    printf("sleep in: %s\n", sleepIn);
+    return 0;
+}
+
